9313c44eed3caf146681c74cffb81d2ea8ad2f27
Diyor Adashev <adashevdiyor443@gmail.com>
2026-04-30 13:17:47 +0500

    add to cart

--- backend/src/controllers/CartController.js
+++ backend/src/controllers/CartController.js
@@ -1,4 +1,5 @@
 import {cartItems, products} from "../data/db.js";
+import {uuidv4} from "uuid";
 
 export function viewCart(req, res) {
     const userId = req.headers('x-user-id');
@@ -13,6 +14,36 @@ export function viewCart(req, res) {
     return res.status(200).json(result);
 }
 export function addToCart(req, res) {
+    const userId = req.headers('x-user-id');
+    const {productId, quantity} = req.body;
+    const product = products.find(p => p.id === productId);
+
+    if (!product) {
+        return res.status(400).json({message: 'Product not found'});
+    }
+
+    if(product.stock<quantity) {
+        return res.status(400).json({message: 'Not enough stock'});
+    }
+
+    const existingCart = cartItems.find(item => item.user_id === userId && item.product_id === productId);
+
+    if (existingCart) {
+        existingCart.quantity += quantity;
+    } else{
+        cartItems.push({
+            id: uuidv4(),
+            user_id: userId,
+            productId: productId,
+            quantity: quantity,
+        });
+    }
+
+    product.stock -= quantity;
+    return res.status(200).json({message: 'Product added successfully'});
+
+
+
 
 }
 export function updateCartItem(req, res) {
