e64b3b49f558b99b4c622f543122afdb21bb11b5
Diyor Adashev <adashevdiyor443@gmail.com>
2026-05-05 12:55:21 +0500

    update cart

--- backend/src/controllers/CartController.js
+++ backend/src/controllers/CartController.js
@@ -28,41 +28,74 @@ export function viewCart(req, res) {
             }
         })
     }
+    return res.status(200).json(cartWithProducts);
 
 }
 export function addToCart(req, res) {
     const userId = req.headers('x-user-id');
     const {productId, quantity} = req.body;
-    const product = products.find(p => p.id === productId);
 
+    const product = products.find(p => p.id === productId);
     if (!product) {
         return res.status(400).json({message: 'Product not found'});
     }
 
-    if(product.stock<quantity) {
-        return res.status(400).json({message: 'Not enough stock'});
-    }
-
-    const existingCart = cartItems.find(item => item.user_id === userId && item.product_id === productId);
-
-    if (existingCart) {
-        existingCart.quantity += quantity;
-    } else{
-        cartItems.push({
+    let userCart = cartItems.find(p => p.user_idid === userId);
+    if (!userCart) {
+        userCart = {
             id: uuidv4(),
             user_id: userId,
-            productId: productId,
-            quantity: quantity,
+            products: [],
+            quantity: 0,
+        }
+        cartItems.push(userCart);
+    }
+
+    const existingProducts = userCart.products.find(
+        (p) => p.id === productId
+    )
+    if (existingProducts) {
+        existingProducts.quantity += 1;
+    }else{
+        userCart.products.push({
+            id: productId,
+            quantity: 1,
         });
     }
 
-    product.stock -= quantity;
-    return res.status(200).json({message: 'Product added successfully'});
+    userCart.quantity = userCart.products.reduce(
+        (sum, item) =>  sum+item.quantity,
+        0,
+        );
+    return res.status(200).json({message: 'Added to cart!'});
 
 
 }
 export function updateCartItem(req, res) {
+    const userId = req.headers('x-user-id');
+    const productId = req.params.id;
+    const {quantity} = req.body;
 
+    if (!quantity || quantity < 1) {
+        return res.status(400).json({message: 'quantity is required'});
+    }
+
+    const userCart = cartItems.find(p => p.user_id === userId);
+    if (!userCart) {
+        return res.status(404).json({message: 'Cart not found!'});
+    }
+
+    const cartProduct = userCart.products.find(p => p.id === productId);
+    if (!cartProduct) {
+        return res.status(404).json({message: 'Product not in cart!'});
+
+    }
+
+    cartProduct.quantity = quantity;
+    userCart.quantity = userCart.products.reduce(
+        (sum, item) =>  sum+item.quantity,
+        0,
+    );
 }
 export function removeCartItem(req, res) {
 
