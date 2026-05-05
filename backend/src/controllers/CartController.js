d23c1ca5f4a8ec74e309a63ab1da006a2132d438
Diyor Adashev <adashevdiyor443@gmail.com>
2026-05-05 13:17:56 +0500

    uuidv4 issue fixed

--- backend/src/controllers/CartController.js
+++ backend/src/controllers/CartController.js
@@ -1,5 +1,5 @@
 import {cartItems, products} from "../data/db.js";
-import {uuidv4} from "uuid";
+import { v4 as uuidv4 } from "uuid";
 
 export function viewCart(req, res) {
     const userId = req.headers('x-user-id');
